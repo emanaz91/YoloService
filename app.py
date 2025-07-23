3ba6102d52d17b66f318e0cab738fcd406450eec
Iman <emanaz91@gmail.com>
2025-07-23 18:17:05 +0300

    fix_5

--- app.py
+++ app.py
@@ -298,7 +298,7 @@ def health():
     """
     Health check endpoint
     """
-    return {"status": "okok"}
+    return {"status": "okokok"}
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8080)
